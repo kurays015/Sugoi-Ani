c7878167ee5daa23fc41a52292c1e5f70e571006
Christzxz <christnarvarte15@gmail.com>
2023-11-28 16:24:59 +0800

    add episode tracker where you left off, to local storage

--- src/components/AnimeEpisodes.jsx
+++ src/components/AnimeEpisodes.jsx
@@ -3,13 +3,31 @@ import { Link } from "react-router-dom";
 
 function AnimeEpisodes() {
   const anime = JSON.parse(localStorage.getItem("anime") || "[]");
-  const [isAlreadySelected, setIsAlreadySelected] = useState({});
-  const [currentEpisode, setCurrentEpisode] = useState(null);
+  // save to local storage - will refactor using useLocalStorage hook
+  const isAlreadySelectedLocalStorage = JSON.parse(
+    localStorage.getItem("isAlreadySelected")
+  );
+  const currentEpisodeLocalStorage = JSON.parse(
+    localStorage.getItem("currentEpisode")
+  );
+  const [isAlreadySelected, setIsAlreadySelected] = useState(
+    isAlreadySelectedLocalStorage || "{}"
+  );
+  const [currentEpisode, setCurrentEpisode] = useState(
+    currentEpisodeLocalStorage || null
+  );
 
   const selectedEpisode = id => {
     setIsAlreadySelected(prev => ({ ...prev, [id]: true }));
     setCurrentEpisode(id);
   };
+  useEffect(() => {
+    localStorage.setItem(
+      "isAlreadySelected",
+      JSON.stringify(isAlreadySelected)
+    );
+    localStorage.setItem("currentEpisode", JSON.stringify(currentEpisode));
+  }, [isAlreadySelected, currentEpisode]);
 
   return (
     <aside className="p-2 bg-[#1C1C1C] max-w-[300px] h-[500px] overflow-y-auto custom-sm:mx-auto custom-sm:w-full custom-sm:max-w-full">
