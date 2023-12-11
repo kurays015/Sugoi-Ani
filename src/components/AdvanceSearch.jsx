d8f7af3bd0cd05fc021a1a06b8892cf68270938d
Christzxz <christnarvarte15@gmail.com>
2023-12-12 01:29:01 +0800

    add reset page in genres search

--- src/components/AdvanceSearch.jsx
+++ src/components/AdvanceSearch.jsx
@@ -2,9 +2,11 @@ import { Button, Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
 import { FaChevronDown } from "react-icons/fa6";
 import { Link, Outlet } from "react-router-dom";
 import { useAdvanceSearchArray } from "../hooks/useAdvanceSearchArray";
+import { useCategoryContext } from "../hooks/useCategoryContext";
 
 function AdvanceSearch() {
   const { genres, type } = useAdvanceSearchArray();
+  const { setPageNumber } = useCategoryContext();
   return (
     <main className="text-center">
       <Menu>
@@ -19,13 +21,18 @@ function AdvanceSearch() {
         <MenuList>
           {genres.map(genre => (
             <MenuItem key={genre}>
-              <Link to={`/advance-search/${genre}`} key={genre}>
+              <Link
+                to={`/advance-search/${genre}`}
+                key={genre}
+                onClick={() => setPageNumber(1)}
+              >
                 {genre}
               </Link>
             </MenuItem>
           ))}
         </MenuList>
       </Menu>
+      <h5 className="text-white my-5">in progress...</h5>
       <Outlet />
     </main>
   );
