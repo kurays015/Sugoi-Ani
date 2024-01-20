a4ace7cb4038a5308d48de9c22df94c36c36b657
Christzxz <christnarvarte15@gmail.com>
2024-01-20 11:39:39 +0800

    fixed save to db when google login used

--- src/hooks/useGoogleCredentials.jsx
+++ src/hooks/useGoogleCredentials.jsx
@@ -3,6 +3,7 @@ import { useGoogleLogin } from "@react-oauth/google";
 import { useNavigate } from "react-router-dom";
 import { setCookies } from "./useCookies";
 import { useGetAnimeDataInLocalStorage } from "./useLocalStorage";
+import axios from "axios";
 
 export default function useGoogleCredentials() {
   const navigate = useNavigate();
@@ -10,8 +11,11 @@ export default function useGoogleCredentials() {
   const anime = useGetAnimeDataInLocalStorage();
 
   const googleLogin = useGoogleLogin({
-    onSuccess: res => {
+    onSuccess: async res => {
       setCookies("googleUser", res.access_token);
+      await axios.post(`${import.meta.env.VITE_BACKEND_GOOGLE}`, {
+        access_token: res.access_token,
+      });
       navigate(`/watch/${anime.episodes[0]?.id}`);
       // window.location.reload();
     },
